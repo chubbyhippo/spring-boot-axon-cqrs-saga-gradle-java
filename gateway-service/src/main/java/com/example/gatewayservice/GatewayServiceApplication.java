3a4223147c0aa769f7d9504ab141167505a9ecdb
ch <mkmsacc@outlook.com>
2023-09-03 13:00:14 +0700

    Configure context propagation for gateway

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -1,15 +1,28 @@
 package com.example.gatewayservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> chain.filter(exchange)
+                .then(Mono.fromRunnable(() -> log.info("Global Post Filter executed")));
+    }
 
 }
